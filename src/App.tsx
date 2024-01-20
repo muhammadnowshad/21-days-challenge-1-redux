ae442ee97530ca0401ec54a22b48e6b42714272f
muhammadnowshad <muhammadnowshad2020@gmail.com>
2024-01-20 15:26:24 +0600

    added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
 
+import { useAppDispatch, useAppSelector } from "./redux/hook";
+import { decrement, increment, incrementByAmount } from "./redux/features/counterSlice/counterSlice";
+
 function App() {
+
+  const { count } = useAppSelector((state) => state.counter);
+  const dispatch = useAppDispatch();
 
   return (
     <>
-      <div>
-        <h1 className="bg-blue-600">Technet</h1>
+      <div className="flex items-center justify-center h-screen">
+        <div className="w-1/2 bg-gray-200 p-10 rounded">
+          <div className="flex justify-center my-4">
+            <button
+              className="px-5 py-2 bg-green-600 rounded mx-5"
+              onClick={() => dispatch(increment())}
+            >
+              Increment
+            </button>
+            <button
+              className="px-5 py-2 bg-green-600 rounded mx-5"
+              onClick={() => dispatch(incrementByAmount(5))}
+            >
+              Increment +5
+            </button>
+            <div className="mx-5 self-center">{count}</div>
+            <button
+              className="px-5 py-2 bg-yellow-600 rounded mx-5"
+              onClick={() => dispatch(decrement())}
+            >
+              Decrement
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
---
+++ src/redux/features/counterSlice/counterSlice.tsx
@@ -0,0 +1,28 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+interface CounterState {
+  count: number;
+}
+
+const initialState: CounterState = {
+  count: 0,
+};
+
+const couterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.count = state.count + 1;
+    },
+    decrement: (state) => {
+      state.count = state.count - 1;
+    },
+    incrementByAmount: (state, action: PayloadAction<number>) => {
+      state.count += action.payload;
+    },
+  },
+});
+
+export const { increment, decrement, incrementByAmount } = couterSlice.actions;
+export default couterSlice.reducer;
